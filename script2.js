c1717c4e9e2986810662ff7dc96edf73c5c0ae34
Mohd. Naved Sheikh <75069324+Naezy26701@users.noreply.github.com>
2021-12-12 08:43:58 +0000

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+const gallery = document.querySelectorAll(".gallery .image"), 
+previewBox = document.querySelector(".preview-box");
+
+window.onload = ()=>{
+    for(let i = 0; i < gallery.length; i++){
+        gallery[i].onclick = ()=>{
+            console.log(i);
+            previewBox.classList.add("show");
+        }
+    }
+}
